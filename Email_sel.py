6328405d44e561ec30462eed44ca3a8d8a938939
Ankit <ankit.tiwari12192@gmail.com>
2016-04-04 16:56:00 +0530

    Can send email with subject now

--- Email_sel.py
+++ Email_sel.py
@@ -6,9 +6,8 @@ prerequisite : The following libraries should be installed
 3. selenium
 4. requests
 step to run
-python facebooklogin.py
+python Email_sel.py
 """
-
 #---------------------------
 #	Importing Modules
 #---------------------------
@@ -21,6 +20,7 @@ from selenium.webdriver.common.keys import Keys
 from requests.exceptions import ConnectionError
 import getpass
 import time
+import random
 
 #Will be converting into modules
 browser = webdriver.Firefox()
@@ -30,25 +30,14 @@ inputElement = browser.find_element_by_name('Email')
 #phone =raw_input("Enter email - > ")
 inputElement.send_keys('ank9222')
 inputElement.send_keys(Keys.ENTER)
-time.sleep(1)
+time.sleep(3)
 inputElement3 = browser.find_element_by_name('Passwd')
 inputElement3.send_keys('qwer12192')
 inputElement3.send_keys(Keys.ENTER)
-time.sleep(5)
-inputElement4=browser.find_element_by_xpath('//*[@id=":jb"]/div').click()
-
-inputElement5=browser.find_element_by_tag_name("textarea").send_keys("ank9222@gmail.com",Keys.ENTER)
-inputElement6=browser.find_element_by_class_name('aoT').send_keys("Testing Automation2",Keys.COMMAND+Keys.ENTER)
-#inputElement7=browser.find_element_by_name("Message ").send_keys("j")#\3a q5
-
-"""
-start_time = time.time()
-browser.find_element_by_tag_name('textarea').is_displayed()
-print("Logged in !")
-print("total --- %s seconds --- taken to login " % (int(time.time() - start_time)*100))
-
-"""
-
-
-#//*[@id=":jk"]/div/div
-
+time.sleep(2)
+for i in range(1,3):
+    inputElement4=browser.find_element_by_xpath('//*[@id=":jb"]/div').click()
+    time.sleep(2)
+    inputElement5=browser.find_element_by_tag_name("textarea").send_keys("ankit.tiwari12192@gmail.com",Keys.ENTER)
+    inputElement6=browser.find_element_by_class_name('aoT').send_keys("Testing Automation "+str(random.randint(1, 1000000)))
+    inputElement7=browser.find_element_by_xpath("//div[@aria-label='Message Body']").send_keys("This is message number "+str(random.randint(1,999922922)),Keys.COMMAND+Keys.ENTER)
